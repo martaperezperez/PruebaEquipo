7de955d8b242c8a512b53cb34038b88f4594a547
Jose Montes Bernardez <jmontesbernardez@danielcastelao.org>
2022-01-14 09:29:36 +0100

    Commit de la prueba de la rama experimental

--- PruebeEquipo/src/PruebaJose.java
+++ PruebeEquipo/src/PruebaJose.java
@@ -2,4 +2,8 @@ public class PruebaJose {
     public void pruebajose(){
         System.out.println("Hola Maquinas");
     }
+
+    public void pruebaBranch(){
+        System.out.println("ESto es una prueba de la rama experimental");
+    }
 }
