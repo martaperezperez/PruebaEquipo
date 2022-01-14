a7da5d130bbb626d7c0403bfb762976b5f1e9857
Alex Fernandez Martinez <afernandezmartinez@danielcastelao.org>
2022-01-14 09:14:14 +0100

    Prueba Alex

---
+++ PruebeEquipo/src/Alex.java
@@ -0,0 +1,5 @@
+public class Alex {
+    public static void pruebaAlex(){
+        System.out.println(0.1+0.2==0.3);
+    }
+}
--- PruebeEquipo/src/PruebaEquipo.java
+++ PruebeEquipo/src/PruebaEquipo.java
@@ -2,6 +2,8 @@ public class PruebaEquipo {
 
     public static void main(String[] args)  {
 
+    Alex.pruebaAlex();
+
     System.out.println("hola");
     System.out.println("hola 2");
 }
